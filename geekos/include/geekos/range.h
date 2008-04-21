8c9ef4266bea1d042cd326c7d16ca35006535387
david.hovemeyer <david.hovemeyer@71cfba67-164b-0410-a1c2-b903225493e6>
2008-04-21 10:25:21 +0000

    initial checkin

---
+++ geekos/include/geekos/range.h
@@ -0,0 +1,27 @@
+/*
+ * GeekOS - range checking and other integer math operations
+ * Copyright (C) 2001-2008, David H. Hovemeyer <david.hovemeyer@gmail.com>
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *   
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *  
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef RANGE_H
+#define RANGE_H
+
+static unsigned range_umin(unsigned a, unsigned b);
+static unsigned range_umax(unsigned a, unsigned b);
+
+#endif /* RANGE_H */
+
