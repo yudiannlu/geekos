8785cea7fad3b8c910c1699f314b31dc05a2f33b
david.hovemeyer <david.hovemeyer@71cfba67-164b-0410-a1c2-b903225493e6>
2008-04-21 11:09:33 +0000

    LBA datatype

---
+++ geekos/include/geekos/lba.h
@@ -0,0 +1,34 @@
+/*
+ * GeekOS - LBA (logical block address) type and operations
+ * Copyright (C) 2001-2008, David H. Hovemeyer <david.hovemeyer@gmail.com>
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *   
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *  
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GEEKOS_LBA_H
+#define GEEKOS_LBA_H
+
+#include <stddef.h>
+#include <geekos/types.h>
+
+/* logical block address type */
+/*typedef u32_t lba_t;*/
+typedef struct { u32_t val; } lba_t;
+
+bool lba_is_range_valid(lba_t start, u32_t num_blocks, u32_t total_blocks);
+size_t lba_block_offset_in_bytes(lba_t lba, unsigned block_size);
+size_t lba_range_size_in_bytes(u32_t num_blocks, unsigned block_size);
+
+#endif /* GEEKOS_LBA_H */
---
+++ geekos/src/kernel/lba.c
@@ -0,0 +1,49 @@
+/*
+ * GeekOS - LBA (logical block address) type and operations
+ * Copyright (C) 2001-2008, David H. Hovemeyer <david.hovemeyer@gmail.com>
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *   
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *  
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <geekos/range.h>
+#include <geekos/lba.h>
+
+/*
+ * Is the range of blocks described by given start address and
+ * number of blocks valid, so that each block in the range
+ * has an address less than the total number of blocks?
+ */
+bool lba_is_range_valid(lba_t start, u32_t num_blocks, u32_t total_blocks)
+{
+	return range_is_valid_u32(start.val, num_blocks, total_blocks);
+}
+
+/*
+ * Get the offset in bytes of a block from the start of the
+ * block device.
+ */
+size_t lba_block_offset_in_bytes(lba_t lba, unsigned block_size)
+{
+	return ((size_t) lba.val) * block_size;
+}
+
+/*
+ * Get total size of a range of blocks in bytes.
+ */
+size_t lba_range_size_in_bytes(u32_t num_blocks, unsigned block_size)
+{
+	return ((size_t) num_blocks) * block_size;
+}
+
